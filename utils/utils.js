39719e2da31eb7aff1da25b577293febd866e230
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-03-12 22:29:55 +0400

    docs: readme update

--- utils/utils.js
+++ utils/utils.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose');
-
 const ApplicationError = require('../errors/ApplicationError');
-
-const { ERR_STATUS_BAD_REQUEST } = require('./constants');
 
 module.exports.appErrorHandler = (err, req, res, next) => {
   // в теории советуют работать именно с инстансами классов
   // а не c именами ошибок
-  if (err instanceof mongoose.Error.CastError) {
-    res.status(ERR_STATUS_BAD_REQUEST).send({ message: err.message });
-    return;
-  }
 
-  //
+  // ApplicationError(500) ошибка по умолчанию и её наследники
+  //  - BadRequestError(400)
+  //  - AuthorizationError(401)
+  //  - DuplicateKeyError(403)
+  //  - NotFoundError(404)
+  //  - WrongCardOwnerError(409)
   if (err instanceof ApplicationError) {
     res.status(err.status).send({ message: err.message });
     return;
   }
 
+  // если новая неисзветная ошибка, то отправим стандартную ApplicationError(500)
   if (err) {
     const defautlError = new ApplicationError();
     res.status(defautlError.status).send({ message: defautlError.message });
