934887a70b706407379e79529e786ab367670841
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-03-25 00:35:46 +0400

    test: cors

--- utils/utils.js
+++ utils/utils.js
@@ -1,16 +1,8 @@
-const mongoose = require('mongoose');
 const ApplicationError = require('../errors/ApplicationError');
-const BadRequestError = require('../errors/BadRequestError');
 
 module.exports.appErrorHandler = (err, req, res, next) => {
   // в теории советуют работать именно с инстансами классов,
   // а не c именами ошибок
-
-  if (err instanceof mongoose.Error.ValidationError) {
-    const badRequestError = new BadRequestError();
-    res.status(badRequestError.status).send({ message: err.message });
-    return;
-  }
 
   // ApplicationError(500) ошибка по умолчанию и её наследники
   //  - BadRequestError(400)
